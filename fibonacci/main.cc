3c22e89f8e0d2657a518d8d011c3479748a434a8
vaibhavhubli <vaibhav.chakki@gmail.com>
2017-09-04 19:28:07 -0700

    added fib using DP

--- fibonacci/main.cc
+++ fibonacci/main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <assert.h>
 using namespace std;
 
 typedef unsigned int uint32;
@@ -23,11 +24,38 @@ uint32 fib2(uint32 n) {
     return sum;
 }
 
+/*
+ * Fib using dynamic programming
+ */
+int N = 100;
+int *lookup = NULL;
+void initialize() {
+  lookup = new int(N);
+  for (int i = 0; i < N; i++) {
+    lookup[i] = -1;
+  }
+}
+
+uint32 fib3(uint32 n) {
+  assert(lookup);
+  if (lookup[n] == -1) {
+    if (n < 2) {
+      lookup[n] = n;
+    }
+    else {
+      lookup[n] = fib3(n-1) + fib3(n-2);
+    }
+  }
+  return lookup[n];
+}
+
 int main(int argc, char** argv) {
     uint32 n;
     cin >> n;
 
+    initialize();
     cout << "Fib 1: " << fib1(n) << endl;
     cout << "Fib 2: " << fib2(n) << endl;
+    cout << "Fib 3: " << fib3(n) << endl;
     return 0;
 }
